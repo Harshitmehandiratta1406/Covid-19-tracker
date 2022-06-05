257c140edc2be7a7177e88507a3807e3809ff4c7
Harshit Mehandiratta <93184048+Harshitmehandiratta1406@users.noreply.github.com>
2022-06-06 01:50:23 +0530

    Update util.js

--- src/util.js
+++ src/util.js
@@ -5,22 +5,35 @@ import { Circle, Popup  } from "react-leaflet";
 const casesTypeColors = {
     cases: {
         hex: "#CC1034",
+        rgb: "(204, 16, 52)",
+        rgba: "(204, 16, 52, 0.5)",
         multiplier: 800,
     },
     recovered: {
         hex: "#7dd71d",
+        rgb: "rgb (125, 68, 67)",
+        half_op: "rgba(125, 215, 29, 0.5)",
         multiplier: 1200,
     },
     deaths: {
         hex: "#fb4443",
+        rgb: "rgb(251, 68, 67)",
+        half_op: "rgba(251, 68, 67, 0.5)",
         multiplier: 2000,
     },
 };
 
 export const sortData = (data) => {
-    const sortedData = [...data];
-
-    return sortedData.sort( (a, b) => (a.cases > b.cases ? -1 : 1));
+    let sortedData = [...data];
+    sortedData.sort((a,b)) => {
+     if(a.cases > b.cases) {
+       return -1;
+     }
+     else {
+      return 1;
+     }
+   });
+   return sortedData;
      
 };
 
